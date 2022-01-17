0834e9a6315424ccb079a6e71fe5351f1da040f6
Thashon Srikantharajah <thashon.s@hotmail.com>
2022-01-16 22:32:22 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskviewComponent } from './pages/taskview/taskview.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path:'', component:TaskviewComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/taskview/taskview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-taskview',
+  templateUrl: './taskview.component.html',
+  styleUrls: ['./taskview.component.scss']
+})
+export class TaskviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
